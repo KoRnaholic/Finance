91af9097994b1f09cce5b9677a17c636413bca04
KoRnaholic <114020851+KoRnaholic@users.noreply.github.com>
2024-09-26 15:53:12 +0400

    created api summary endpoint for transactions

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,11 +1,10 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-import { z } from "zod";
-import { zValidator } from "@hono/zod-validator";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import accounts from "./accounts";
 import categories from "./categories";
 import transactions from "./transactions";
+import summary from "./summary";
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
@@ -27,7 +26,8 @@ app.get("/hello", clerkMiddleware(), (c) => {
 const routes = app
   .route("/accounts", accounts)
   .route("/categories", categories)
-  .route("/transactions", transactions);
+  .route("/transactions", transactions)
+  .route("/summary", summary);
 
 export const GET = handle(app);
 export const POST = handle(app);
