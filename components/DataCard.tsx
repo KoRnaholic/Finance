f32be3d475202e63dfa5028ba4e4d13454cc09dc
KoRnaholic <114020851+KoRnaholic@users.noreply.github.com>
2024-10-01 15:38:26 +0400

    created data coiuntup component for dashboard and added skeleton

--- components/DataCard.tsx
+++ components/DataCard.tsx
@@ -7,7 +7,9 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { cn } from "@/lib/utils";
+import { cn, formatCurreny, formatPercentage } from "@/lib/utils";
+import { CountUp } from "./CountUp";
+import { Skeleton } from "./ui/skeleton";
 
 const boxVariant = cva("rounded-md p3", {
   variants: {
@@ -73,6 +75,45 @@ export default function DataCard({
           <Icon className={cn(iconVariant({ variant }))} />
         </div>
       </CardHeader>
+      <CardContent>
+        <h1 className="font-bold text-2xl mb-2 line-clamp-1 break-all">
+          <CountUp
+            preserveValue
+            start={0}
+            end={value}
+            decimals={2}
+            decimalPlaces={2}
+            formattingFn={formatCurreny}
+          />
+        </h1>
+        <p
+          className={cn(
+            "text-muted-foreground text-sm line-clamp-1",
+            percentageChange > 0 && "text-emerald-500",
+            percentageChange < 0 && "text-rose-500"
+          )}
+        >
+          {formatPercentage(percentageChange)} from last period
+        </p>
+      </CardContent>
     </Card>
   );
 }
+
+export const DataCardLoading = () => {
+  return (
+    <Card className="borer-none drop-shadow-sm h-[192px]">
+      <CardHeader className="flex flex-row items-center jsutify-between gap-x-4">
+        <div className="space-y-2">
+          <Skeleton className="h-6 w-24" />
+          <Skeleton className="h-4 w-40" />
+        </div>
+        <Skeleton className="size-12" />
+      </CardHeader>
+      <CardContent>
+        <Skeleton className="shrink-0 h-10 w-24 mb-2" />
+        <Skeleton className="shrink-0 h-4 w-40" />
+      </CardContent>
+    </Card>
+  );
+};
